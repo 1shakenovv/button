5abad25567b400da3f823d3cb9035e27088c940e
Shakenov Bakht1arr <55362290+1shakenovv@users.noreply.github.com>
2022-06-02 20:40:50 +0600

    Add files via upload

---
+++ next.js
@@ -0,0 +1,16 @@
+const crazyButtons = document.querySelectorAll(".btn-crazy");
+
+function goCrazy() {
+
+  const offsetLeft = Math.random() * (window.innerWidth - this.clientWidth);
+  const offsetTop = Math.random() * (window.innerHeight - this.clientHeight);
+
+  console.log(offsetLeft, offsetTop);
+
+  this.style.top = offsetTop + "px";
+  this.style.left = offsetLeft + "px";
+}
+
+crazyButtons.forEach((button) =>
+  button.addEventListener("mouseenter", goCrazy)
+);
